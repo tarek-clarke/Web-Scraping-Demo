1a92eaf0e3a693321e84fb7e0d66f3e6b435be7c
tarek-clarke <tarek.clarke15@gmail.com>
2026-07-19 21:23:59 -0400

    chore: initial tkde branch - pruned web apps, legacy tools, and non-paper report assets

--- go/ingestion/types.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Packet struct {
-	Source    string                 `json:"source"`
-	Timestamp string                 `json:"timestamp"`
-	Data      map[string]interface{} `json:"data"`
-}
